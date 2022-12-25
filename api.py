a4aaa2fac9918cc3d908ca6f5c553bf6785a6187
NINEZERN <gleb.devyatka@gmail.com>
2022-12-26 00:28:13 +0200

    Added a season working.

--- api.py
+++ api.py
@@ -1,5 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
+from requests import Response
+from typing import NamedTuple, Union
+from fake_useragent import UserAgent
+
+
+class Episode(NamedTuple):
+    title: str
+    link: str
+
+
+class Season(NamedTuple):
+    title: str
+    link: str
 
 
 class Anime:
@@ -7,17 +20,17 @@ class Anime:
     def __init__(self, title, link) -> None:
         self.title = title
         self.link = link
+        self.ua = UserAgent()
         self.HEADERS = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"
+            "User-Agent": self.ua.random
         }
-    
 
     def _get_description(self):
         r = requests.get(self.link, headers=self.HEADERS)
         soup = BeautifulSoup(r.text, 'html.parser')
-        description = [span for span in soup.find('p', class_="under_video uv_rounded_bottom the_hildi").find_all("span")][0]
+        description = \
+            [span for span in soup.find('p', class_="under_video uv_rounded_bottom the_hildi").find_all("span")][0]
         return description
-
 
     def _get_image(self):
         r = requests.get(self.link, headers=self.HEADERS)
@@ -25,13 +38,19 @@ class Anime:
         image = soup.find("div", class_="all_anime_title").get("style").split(")")[0].split("(")[-1].replace("'", "")
         return image
 
+    def get_episodes(self, season: Season) -> list[Episode]:
+        r = requests.get(season.link, headers=self.HEADERS)
+        soup = BeautifulSoup(r.text, 'html.parser')
+        episodes = [Episode(title=episode.text, link=episode.get("href")) for episode in
+                    soup.find('div', class_="watch_l").find_all("a")][2:]
+        return episodes
 
     def get_seasons(self):
         r = requests.get(self.link, headers=self.HEADERS)
         soup = BeautifulSoup(r.text, 'html.parser')
-        seasons = [season.text for season in soup.find_all("h2", class_="b-b-title the-anime-season center")]
+        seasons = [Season(link="https://jut.su" + season.find("a").get("href"), title=season.find("a").text) for season
+                   in soup.find_all("div", class_="the_invis")]
         return seasons
-
 
     def _get_video_url(self, page_url: str) -> str:
         r = requests.get(page_url, headers=self.HEADERS)
@@ -39,59 +58,51 @@ class Anime:
         video_link = soup.find(id='my-player').find_all('source')[1].get('src')
         return video_link
 
-    
     def _download(self, video_link: str, path: str):
         vid = requests.get(video_link, headers=self.HEADERS)
         with open(path, 'wb') as f:
             f.write(vid.content)
-        
-        
-    def download(self, epizode, path: str):
-        page_url = f"{self.link}episode-{epizode}.html"
+
+    def download(self, episode: int, path: str, season: int = None):
+        if season is None:
+            page_url = f"{self.link}episode-{episode}.html"
+        else:
+            page_url = f"{season}episode-{episode}.html"
         video_link = self._get_video_url(page_url)
         video_name = video_link.split('/')[-1].split("?")[0]
-        self._download(video_link, path=path+video_name)
+        self._download(video_link, path=path + video_name)
 
-
-        
-
-
-        
-        
 
 class Jutsu:
 
     def __init__(self) -> None:
-        pass
+        self.ua = UserAgent()
 
-    
-    def _parse_anime(self,response: str) -> list[Anime]:
+    def _parse_anime(self, response: Response) -> list[Anime]:
         """Taking html and returning list of `Anime`"""
         soup = BeautifulSoup(response.text, 'html.parser')
         titles = [title.text for title in soup.find_all("div", class_="aaname")]
-        links = ["https://jut.su"+link.find("a").get("href") for link in soup.find_all("span", class_="the_invis")]
-        
-        animes = []        
+        links = ["https://jut.su" + link.find("a").get("href") for link in soup.find_all("span", class_="the_invis")]
+
+        animes = []
         for i in range(len(titles)):
             animes.append(Anime(title=titles[i], link=links[i]))
         return animes
 
-
     def search(self, anime: str) -> list[Anime]:
-        """Taking an anime, than parsing html from Jutsu"""
+        """Taking an anime, then parsing html from Jutsu"""
         url = "https://jut.su/anime/"
         headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
+            "User-Agent": self.ua.random
         }
         s = requests.Session()
         data = {
-            'ajax_load':  'yes',
-            'start_from_page':  1,
-            'show_search':  anime,
+            'ajax_load': 'yes',
+            'start_from_page': 1,
+            'show_search': anime,
             'anime_of_user': ''
         }
-        r = s.get(url=url, headers=headers)
+        s.get(url=url, headers=headers)
         post_request = s.post(url, data, headers=headers)
         animes = self._parse_anime(post_request)
         return animes
-    
--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 from api import Jutsu
 from tqdm import tqdm
 
+
 class ChoiceError(Exception):
     """Raise if you wrote a wrong choice"""
 
+
 def _print_anime(animes: list) -> None:
     for n, anime in enumerate(animes):
-        print (f"[{n+1}] [{anime.title} - {anime.link}]")
+        print(f"[{n + 1}] [{anime.title}]")
 
-    
+
 def parse_epizode(user_input) -> list[int]:
-    
     choosed = []
-    
-    if 2%1: pass  #user_input.strip().lower() == 'all':
-        #animes = targets[:]
+
+    if 2 % 1:
+        pass  # user_input.strip().lower() == 'all':
+    # animes = targets[:]
     else:
         for r in user_input.split(','):
             r = r.strip()
@@ -31,24 +33,57 @@ def parse_epizode(user_input) -> list[int]:
                 choosed.append(int(r) - 1)
     return choosed
 
+
+def tes_episodes():
+    jutsu = Jutsu()
+
+    # anime
+    animes = jutsu.search("клинок")
+    anime = animes[0]
+
+    # seasone
+    seasons = anime.get_seasons()
+    if seasons:
+        season = 1
+        # _print_anime(seasons)
+        # season = seasons[int(input(f"Write an index btween of 1 - {len(seasons)} ->")) - 1]
+        # print(season.link)
+
+    # episode
+    ep = anime.get_episodes(seasons[season])
+    for i in ep:
+        print(i)
+
+
 def main():
     jutsu = Jutsu()
+
+    # anime
     animes = jutsu.search(input("Write an anime ->"))
     # animes = jutsu.search('хантер')
     _print_anime(animes=animes)
+    anime = animes[int(input(f"Write an index btween of 1 - {len(animes)} ->")) - 1]
 
-    anime = int(input(f"Write an index btween of 1 - {len(animes)} ->")) - 1
+    # seasone
+    seasons = anime.get_seasons()
+    if seasons:
+        _print_anime(seasons)
+        season = seasons[int(input(f"Write an index btween of 1 - {len(seasons)} ->")) - 1]
+        print(season.link)
 
-    epizode = input("Write smthing ->")
-    epizodes = parse_epizode(epizode)
-    
+    # episode
+    ep = anime.get_episodes(season)
+    print(ep)
+    episode = input("Write an episode ->")
+    episodes = parse_epizode(episode)
 
-    for epizode in tqdm(epizodes):
-        anime.download(epizode=epizode, path="Anime/")
+    for episode in tqdm(episodes):
+        if seasons:
+            anime.download(episode=episode, path="Anime/", season=season.link)
+        else:
+            anime.download(episode=episode, path="Anime/")
 
-
-
-        
 
 if __name__ == "__main__":
+    # tes_episodes()
     main()
